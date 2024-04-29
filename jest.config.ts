e057a9d2ffbafeefcfcffd349063df014e3cb45f
Esdras Silva <esdraslimasilva83@gmail.com>
2024-04-29 14:44:08 -0300

    Setup initil tests

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "jest";
+
+const config: Config = {
+    clearMocks: true,
+    testMatch: ["**/__test__/**"],
+};
+
+export default config;
---
+++ src/__test__/dummy.spec.ts
@@ -0,0 +1,3 @@
+it("Should work dummy", () => {
+    expect(1).toBe(1);
+});
